af245535991222bfbfdf8f941b4a59ca62404e43
jwaine44 <jwaine44@gmail.com>
2022-11-29 15:50:10 -0500

    Added ShuffleString

---
+++ ShuffleString.js
@@ -0,0 +1,31 @@
+/*
+You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+
+Example 1:
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+Example 2:
+Input: s = "abc", indices = [0,1,2]
+Output: "abc"
+Explanation: After shuffling, each character remains in its position.
+*/
+
+function restoreString(s, indices){
+    let output = new Array(indices.length);
+    for(let i = 0; i < indices.length; i++){
+        output[indices[i]] = s[i];
+    }
+    return output.join("");
+}
+
+let s1 = "codeleet";
+let s2 = "abc";
+let indices1 = [4,5,6,7,0,2,1,3];
+let indices2 = [0,1,2];
+
+console.log(restoreString(s1, indices1));
+console.log(restoreString(s2, indices2));
