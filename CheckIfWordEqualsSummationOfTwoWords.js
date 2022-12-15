66c07459f81354ca039b91e8a22bb3256f225014
jwaine44 <jwaine44@gmail.com>
2022-12-15 16:37:28 -0500

    Added CheckIfWordEqualsSummationOfTwoWords

---
+++ CheckIfWordEqualsSummationOfTwoWords.js
@@ -0,0 +1,66 @@
+/*
+The letter value of a letter is its position in the alphabet starting from 0 (i.e. 'a' -> 0, 'b' -> 1, 'c' -> 2, etc.).
+
+The numerical value of some string of lowercase English letters s is the concatenation of the letter values of each letter in s, which is then converted into an integer.
+
+For example, if s = "acb", we concatenate each letter's letter value, resulting in "021". After converting it, we get 21.
+You are given three strings firstWord, secondWord, and targetWord, each consisting of lowercase English letters 'a' through 'j' inclusive.
+
+Return true if the summation of the numerical values of firstWord and secondWord equals the numerical value of targetWord, or false otherwise.
+
+Example 1:
+Input: firstWord = "acb", secondWord = "cba", targetWord = "cdb"
+Output: true
+Explanation:
+The numerical value of firstWord is "acb" -> "021" -> 21.
+The numerical value of secondWord is "cba" -> "210" -> 210.
+The numerical value of targetWord is "cdb" -> "231" -> 231.
+We return true because 21 + 210 == 231.
+
+Example 2:
+Input: firstWord = "aaa", secondWord = "a", targetWord = "aab"
+Output: false
+Explanation: 
+The numerical value of firstWord is "aaa" -> "000" -> 0.
+The numerical value of secondWord is "a" -> "0" -> 0.
+The numerical value of targetWord is "aab" -> "001" -> 1.
+We return false because 0 + 0 != 1.
+
+Example 3:
+Input: firstWord = "aaa", secondWord = "a", targetWord = "aaaa"
+Output: true
+Explanation: 
+The numerical value of firstWord is "aaa" -> "000" -> 0.
+The numerical value of secondWord is "a" -> "0" -> 0.
+The numerical value of targetWord is "aaaa" -> "0000" -> 0.
+We return true because 0 + 0 == 0.
+*/
+
+function isSumEqual(firstWord, secondWord, targetWord){
+    let firstWordResult = "";
+    let secondWordResult = "";
+    let targetWordResult = "";
+    for(let i = 0; i < firstWord.length; i++){
+        firstWordResult += firstWord[i].charCodeAt(0) - 97;
+    }
+    for(let j = 0; j < secondWord.length; j++){
+        secondWordResult += secondWord[j].charCodeAt(0) - 97;
+    }
+    for(let k = 0; k < targetWord.length; k++){
+        targetWordResult += targetWord[k].charCodeAt(0) - 97;
+    }
+    
+    return (Number(firstWordResult) + Number(secondWordResult)) == Number(targetWordResult);
+}
+
+let word1 = "acb";
+let word2 = "cba";
+let word3 = "cdb";
+let word4 = "aaa";
+let word5 = "a";
+let word6 = "aab";
+let word7 = "aaaa";
+
+console.log(isSumEqual(word1, word2, word3));
+console.log(isSumEqual(word4, word5, word6));
+console.log(isSumEqual(word4, word5, word7));
