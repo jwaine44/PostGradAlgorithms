8edca4e1e98dbc1da9b36a2060f2a9ea13fff3fd
jwaine44 <jwaine44@gmail.com>
2023-01-18 16:51:09 -0500

    Added NumberOfCommonFactors

---
+++ NumberOfCommonFactors.js
@@ -0,0 +1,36 @@
+/*
+Given two positive integers a and b, return the number of common factors of a and b.
+
+An integer x is a common factor of a and b if x divides both a and b.
+
+Example 1:
+Input: a = 12, b = 6
+Output: 4
+Explanation: The common factors of 12 and 6 are 1, 2, 3, 6.
+
+Example 2:
+Input: a = 25, b = 30
+Output: 2
+Explanation: The common factors of 25 and 30 are 1, 5.
+*/
+
+function commonFactors(a, b){
+    let count = 0;
+    if(a > b){
+        for(let i = 1; i <= a; i++){
+            if(a % i == 0 && b % i == 0){
+                count++;
+            }
+        }
+    } else {
+        for(let j = 1; j <= b; j++){
+            if(a % j == 0 && b % j == 0){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+console.log(commonFactors(12, 6));
+console.log(commonFactors(25, 30));
