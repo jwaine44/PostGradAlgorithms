134f572ff3932a452c9766cf0cc351867187672d
jwaine44 <jwaine44@gmail.com>
2023-03-01 16:36:05 -0500

    Added SortanArray

---
+++ SortanArray.js
@@ -0,0 +1,44 @@
+/*
+Given an array of integers nums, sort the array in ascending order and return it.
+
+You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+Example 1:
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+
+Example 2:
+Input: nums = [5,1,1,2,0,0]
+Output: [0,0,1,1,2,5]
+Explanation: Note that the values of nums are not necessairly unique.
+*/
+
+function sortedArr(numArr){
+    for(let i = 0; i < numArr.length - 1; i++){
+        if(numArr[i] > numArr[i + 1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+function sortArray(nums){
+    let temp = 0;
+    while(!sortedArr(nums)){
+        for(let i = 0; i < nums.length; i++){
+            if(nums[i] >= nums[i + 1]){
+                temp = nums[i + 1];
+                nums[i + 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+    }
+    return nums;
+}
+
+let nums1 = [5,2,3,1];
+let nums2 = [5,1,1,2,0,0];
+
+console.log(sortArray(nums1));
+console.log(sortArray(nums2));
