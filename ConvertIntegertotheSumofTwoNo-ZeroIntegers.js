bd31c0b0d94228a31c01c5df503985aae5eda259
jwaine44 <jwaine44@gmail.com>
2023-03-08 16:05:21 -0500

    Added ConvertIntegertotheSumofTwoNo-ZeroIntegers

---
+++ ConvertIntegertotheSumofTwoNo-ZeroIntegers.js
@@ -0,0 +1,40 @@
+/*
+No-Zero integer is a positive integer that does not contain any 0 in its decimal representation.
+
+Given an integer n, return a list of two integers [a, b] where:
+
+a and b are No-Zero integers.
+a + b = n
+The test cases are generated so that there is at least one valid solution. If there are many valid solutions, you can return any of them.
+
+Example 1:
+Input: n = 2
+Output: [1,1]
+Explanation: Let a = 1 and b = 1.
+Both a and b are no-zero integers, and a + b = 2 = n.
+
+Example 2:
+Input: n = 11
+Output: [2,9]
+Explanation: Let a = 2 and b = 9.
+Both a and b are no-zero integers, and a + b = 9 = n.
+Note that there are other valid answers as [8, 3] that can be accepted.
+*/
+
+function getNoZeroIntegers(n){
+    for(let i = n - 1; i >= 0; i--){
+        if(i.toString().includes("0")){
+            continue;
+        } else {
+            let temp = n - i;
+            if(temp.toString().includes("0")){
+                continue;
+            } else {
+                return [i, temp];
+            }
+        }
+    }
+}
+
+console.log(getNoZeroIntegers(2));
+console.log(getNoZeroIntegers(11));
