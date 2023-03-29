eafa8141aef21064ebadf0e4e44372b0a6d8b15f
jwaine44 <jwaine44@gmail.com>
2023-03-29 15:53:08 -0400

    Added ReducingDishes

---
+++ ReducingDishes.js
@@ -0,0 +1,58 @@
+/*
+A chef has collected data on the satisfaction level of his n dishes. Chef can cook any dish in 1 unit of time.
+
+Like-time coefficient of a dish is defined as the time taken to cook that dish including previous dishes multiplied by its satisfaction level i.e. time[i] * satisfaction[i].
+
+Return the maximum sum of like-time coefficient that the chef can obtain after dishes preparation.
+
+Dishes can be prepared in any order and the chef can discard some dishes to get this maximum value.
+
+Example 1:
+Input: satisfaction = [-1,-8,0,5,-9]
+Output: 14
+Explanation: After Removing the second and last dish, the maximum total like-time coefficient will be equal to (-1*1 + 0*2 + 5*3 = 14).
+Each dish is prepared in one unit of time.
+
+Example 2:
+Input: satisfaction = [4,3,2]
+Output: 20
+Explanation: Dishes can be prepared in any order, (2*1 + 3*2 + 4*3 = 20)
+
+Example 3:
+Input: satisfaction = [-1,-4,-5]
+Output: 0
+Explanation: People do not like the dishes. No dish is prepared.
+*/
+
+function sumArr(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+
+function maxSatisfaction(satisfaction){
+    let maxArr = [];
+    let max = 0;
+    satisfaction = satisfaction.sort((a, b) => a - b);
+    while(satisfaction.length !== 0){
+        for(let i = 0; i < satisfaction.length; i++){
+            maxArr.push((satisfaction[i] * (i + 1)));
+        }
+        if(sumArr(maxArr) > max){
+            max = sumArr(maxArr);
+        }
+        satisfaction.splice(0, 1);
+        maxArr = [];
+    }
+    return max;
+}
+
+let satisfaction1 = [-1,-8,0,5,-9];
+let satisfaction2 = [4,3,2];
+let satisfaction3 = [-1,-4,-5];
+
+console.log(maxSatisfaction(satisfaction1));
+console.log(maxSatisfaction(satisfaction2));
+console.log(maxSatisfaction(satisfaction3));
